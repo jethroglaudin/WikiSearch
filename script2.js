5bb71a41d5bacd02920d80621bf885c720e7fa73
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-23 18:40:56 +0000

    axios js added

---
+++ script2.js
@@ -0,0 +1,39 @@
+/*global axios*/
+// const axios = require('axios');
+function handleClick() {
+    console.log("button is clicked");
+    makeAPICall();
+}
+
+function makeAPICall(){
+    const input = document.getElementById("input").value;
+    const url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=${input}&limit=10&namespace=0&format=json`;
+    axios.get(url)
+        .then(function(response){
+            displayResults(response.data[1], response.data[2], response.data[3]);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
+    //   fetch(url)
+    //     .then(function(response) {
+    //         return response.json()
+    //     }).then(function(response) {
+    //         console.log(response)
+    //         displayResults(response[1], response[2], response[3]);
+    //     })
+
+}
+
+function displayResults(titles,description, links) {
+    const newResponse = titles.map(function(item, index) {
+        console.log(description);
+        // return "<li>" + "<a href="+ links[index]+">" +item+ "</li>"
+        return `<li><a href=${links[index]}>${item}</a><br>${description}</li>`;
+    });
+    const joinedResponse = newResponse.join("\n");
+    console.log(joinedResponse);
+
+    document.getElementById('results').innerHTML = joinedResponse;
+}
